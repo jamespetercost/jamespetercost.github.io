054c7a39755a240187801a4fb78eb101e4ce31e1
Jason Bordeaux <jasonbordeaux@theMacBookPro.local>
2017-05-04 21:23:37 -0700

    updated a few files 05/04/17

---
+++ creeksideicehouse_files/creeksideicehouse.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20170504
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('creeksideicehouse_files/creeksideicehouseMoz.css')
+detectBrowser();fixAllIEPNGs('Media/transparent.gif');IMpreload('creeksideicehouse_files','shapeimage_3','0');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ foreverpebble_files/foreverpebble.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20170504
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('foreverpebble_files/foreverpebbleMoz.css')
+detectBrowser();fixAllIEPNGs('Media/transparent.gif');IMpreload('foreverpebble_files','shapeimage_3','0');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
